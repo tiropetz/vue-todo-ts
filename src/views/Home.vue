3e551cc0de4e1021c5bb4de0da0b1d8e5f70fb06
kentaro_matsutani <kentaro_matsutani@bita.jp>
2020-09-08 12:19:34 +0900

    feat: add todo state

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,35 @@
 <template>
   <div class="container">
     <div>
-      <TaskRow title="へいへい！" isChecked />
-      <TaskRow title="xxをやる" />
-      <TaskRow title="これがタスク" />
+      <TaskRow
+        v-for="todo in todoList"
+        :key="todo.id"
+        :title="todo.title"
+        :is-checked="todo.isChecked"
+      />
     </div>
   </div>
 </template>
 
 <script>
 import TaskRow from "@/components/TaskRow.vue";
+import Vue from "vue";
 
-export default {
+export default Vue.extend({
   name: "Home",
   components: {
     TaskRow
+  },
+  data() {
+    return {
+      todoList: [
+        { id: 1, title: "aaaa", isChecked: true },
+        { id: 2, title: "bbb", isChecked: true },
+        { id: 3, title: "cc", isChecked: false }
+      ]
+    };
   }
-};
+});
 </script>
 
 <style scoped lang="scss">
